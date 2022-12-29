b0b3dca15bb6e0a56f2c9e9b3c1ba67be7d66e91
Lucas Mathieu <lucas.mathieu1@etu.univ-grenoble-alpes.fr>
2022-12-29 13:57:46 +0100

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<main class="overflow-hidden bg-black">
+  <div id="home" class="relative bg-black min-h-screen min-w-screen flex items-center justify-center px-16 overflow-hidden">
+    <div class="relative w-full max-w-3xl">
+      <div class="absolute left-20 top-10 md:top-10 md:left-60 w-64 h-64 bg-blue-300 rounded-full mix-blend-multiply filter blur-xl opacity-65 animate-blob"></div>
+      <div class="absolute -top-20 -right-20 md:-top-10 md:-right-5 w-64 h-64 bg-orange-300 rounded-full mix-blend-multiply filter blur-xl opacity-65 animate-blob animation-delay-2000 pr-[200px]"></div>
+      <div class="absolute -top-40 -left-20 md:-top-20 md:left-10 w-64 h-64 bg-purple-300 rounded-full mix-blend-multiply filter blur-xl opacity-65 animate-blob animation-delay-4000 pr-10"></div>
+      <div class="m-8 relative space-y-10 w-3/4 flex justify-center mx-auto text-ellipsis">
+        <div class="mockup-code w-fit animated bg-transparent border-2 border-base-200 truncate text-ellipsis">
+          <pre data-prefix="$"><code class="long-text">My name is Lucas</code><code class="short-text">My name is Lucas</code></pre>
+          <pre data-prefix="$"><code class="long-text">I am a computer engineering student from France</code><code class="short-text">I'm a french CS student</code></pre>
+          <pre data-prefix="$"><code class="long-text">I like to build apps that live on the web</code><code class="short-text">I make pretty web apps</code></pre>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <script
+    defer
+    src="https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js"></script>
+</main>
